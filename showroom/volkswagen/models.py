e509aea6277bd43eaae73152ced5b10bed983950
VictoriaGailitis <heyleadrightnow@gmail.com>
2023-12-06 09:45:04 +0300

    База данных

--- showroom/volkswagen/models.py
+++ showroom/volkswagen/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class ORGANIZATION(models.Model):
+    NAME_ORG = models.CharField(max_length=30)
+    FIO_LEAD = models.CharField(max_length=45)
+    PH_NUM = models.CharField(max_length=15)
+    ADDRESS = models.CharField(max_length=45)
+    ORGANIZATION_ID = models.IntegerField()
